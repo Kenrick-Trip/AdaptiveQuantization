9185bc76c678836f7b1a5501a98c4f3b0ab5ab59
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-24 22:15:30 +0200

    more refactor

--- Train/blog_main.py
+++ Train/blog_main.py
@@ -8,9 +8,13 @@ from Train.utils import measure_inference_latency, load_torchscript_model, evalu
     calibrate_model, model_equivalence, load_model, set_random_seeds, prepare_dataloader, create_model
 
 
-class QuantizedResNet18(nn.Module):
+class QuantizedResNet(nn.Module):
+    """
+    Helper class for static quantization. This inserts observers in
+    the model that will observe activation tensors during calibration.
+    """
     def __init__(self, model_fp32):
-        super(QuantizedResNet18, self).__init__()
+        super(QuantizedResNet, self).__init__()
         # QuantStub converts tensors from floating point to quantized.
         # This will only be used for inputs.
         self.quant = torch.quantization.QuantStub()
@@ -29,6 +33,26 @@ class QuantizedResNet18(nn.Module):
         # to floating point in the quantized model
         x = self.dequant(x)
         return x
+
+
+def create_fused_model(original_model):
+    fused_model = copy.deepcopy(original_model)
+
+    # The model has to be switched to evaluation mode before any layer fusion.
+    # Otherwise the quantization will not work correctly.
+    fused_model.eval()
+
+    # Fuse the model in place rather manually.
+    fused_model = torch.quantization.fuse_modules(fused_model, [["conv1", "bn1", "relu"]], inplace=True)
+    for module_name, module in fused_model.named_children():
+        if "layer" in module_name:
+            for basic_block_name, basic_block in module.named_children():
+                torch.quantization.fuse_modules(basic_block, [["conv1", "bn1", "relu1"], ["conv2", "bn2"]],
+                                                inplace=True)
+                for sub_block_name, sub_block in basic_block.named_children():
+                    if sub_block_name == "downsample":
+                        torch.quantization.fuse_modules(sub_block, [["0", "1"]], inplace=True)
+    return fused_model
 
 
 def inference():
@@ -52,29 +76,9 @@ def inference():
     model = load_model(model=model, model_filepath=model_filepath, device=cuda_device)
     # Move the model to CPU since static quantization does not support CUDA currently.
     model.to(cpu_device)
-    # Make a copy of the model for layer fusion
-    fused_model = copy.deepcopy(model)
 
+    fused_model = create_fused_model(original_model=model)
     model.eval()
-    # The model has to be switched to evaluation mode before any layer fusion.
-    # Otherwise the quantization will not work correctly.
-    fused_model.eval()
-
-    # Fuse the model in place rather manually.
-    fused_model = torch.quantization.fuse_modules(fused_model, [["conv1", "bn1", "relu"]], inplace=True)
-    for module_name, module in fused_model.named_children():
-        if "layer" in module_name:
-            for basic_block_name, basic_block in module.named_children():
-                torch.quantization.fuse_modules(basic_block, [["conv1", "bn1", "relu1"], ["conv2", "bn2"]],
-                                                inplace=True)
-                for sub_block_name, sub_block in basic_block.named_children():
-                    if sub_block_name == "downsample":
-                        torch.quantization.fuse_modules(sub_block, [["0", "1"]], inplace=True)
-
-    # Print FP32 model.
-    print(model)
-    # Print fused model.
-    print(fused_model)
 
     input_size = (1, 1, 32, 32)
     # Model and fused model should be equivalent.
@@ -82,17 +86,11 @@ def inference():
                              num_tests=100,
                              input_size=input_size), "Fused model is not equivalent to the original model!"
 
-    # Prepare the model for static quantization. This inserts observers in
-    # the model that will observe activation tensors during calibration.
-    quantized_model = QuantizedResNet18(model_fp32=fused_model)
+    # Prepare the model for static quantization.
+    quantized_model = QuantizedResNet(model_fp32=fused_model)
     # Using un-fused model will fail.
     # Because there is no quantized layer implementation for a single batch normalization layer.
-    # quantized_model = QuantizedResNet18(model_fp32=model)
-    # Select quantization schemes from
-    # https://pytorch.org/docs/stable/quantization-support.html
-    # quantization_config = torch.quantization.get_default_qconfig("fbgemm")
-    # Custom quantization configurations
-    # quantization_config = torch.quantization.default_qconfig
+
     quantization_config = torch.quantization.QConfig(
         activation=torch.quantization.MinMaxObserver.with_args(dtype=torch.quint8),
         weight=torch.quantization.MinMaxObserver.with_args(dtype=torch.qint8, qscheme=torch.per_tensor_affine)
@@ -110,14 +108,7 @@ def inference():
 
     quantized_model = torch.quantization.convert(quantized_model, inplace=True)
 
-    # Using high-level static quantization wrapper
-    # The above steps, including torch.quantization.prepare, calibrate_model, and torch.quantization.convert, are also equivalent to
-    # quantized_model = torch.quantization.quantize(model=quantized_model, run_fn=calibrate_model, run_args=[train_loader], mapping=None, inplace=False)
-
     quantized_model.eval()
-
-    # Print quantized model.
-    print(quantized_model)
 
     # Save quantized model.
     save_torchscript_model(model=quantized_model, model_dir=model_dir, model_filename=quantized_model_filename)
@@ -128,9 +119,6 @@ def inference():
     _, fp32_eval_accuracy = evaluate_model(model=model, test_loader=test_loader, device=cpu_device, criterion=None)
     _, int8_eval_accuracy = evaluate_model(model=quantized_jit_model, test_loader=test_loader, device=cpu_device,
                                            criterion=None)
-
-    # Skip this assertion since the values might deviate a lot.
-    # assert model_equivalence(model_1=model, model_2=quantized_jit_model, device=cpu_device, rtol=1e-01, atol=1e-02, num_tests=100, input_size=(1,3,32,32)), "Quantized model deviates from the original model too much!"
 
     print("FP32 evaluation accuracy: {:.3f}".format(fp32_eval_accuracy))
     print("INT8 evaluation accuracy: {:.3f}".format(int8_eval_accuracy))
