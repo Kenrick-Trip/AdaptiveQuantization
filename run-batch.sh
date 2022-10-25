6f7ff2f042b757d55a7d7a08f0d838b45bb85f65
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-25 23:35:40 +0200

    loop over model_names

--- run-batch.sh
+++ run-batch.sh
@@ -1,19 +1,11 @@
-PS3="Enter a number: "
-
-select modelname in resnet18 resnet34; do
-  if test -z "$modelname"; then
-    echo "No model selected"
-  else
-    echo "Selected model: $modelname"
-    break
-  fi
-done
-for cpusiz in 4 5 6; do
-  for memsiz in 1024MB 512MB; do
-    for batchsiz in 1 2 3; do
-      for quantlv in None symmetric affine histogram; do
-        echo "Running $modelname with cpu: $cpusiz, memory: $memsiz, batch size: $batchsiz and quant: $quantlv"
-                docker run -v quantresults:/resultsets --memory=$memsiz --cpus=$cpusiz --rm quantizationtester $modelname $cpusiz $memsiz $batchsiz $quantlv
+for modelname in resnet18 resnet34; do
+  for cpusiz in 4 5 6; do
+    for memsiz in 1024MB 512MB; do
+      for batchsiz in 1 2 4; do
+        for quantlv in None symmetric affine histogram; do
+          echo "Running $modelname with cpu: $cpusiz, memory: $memsiz, batch size: $batchsiz and quant: $quantlv"
+                  docker run -v quantresults:/resultsets --memory=$memsiz --cpus=$cpusiz --rm quantizationtester $modelname $cpusiz $memsiz $batchsiz $quantlv
+        done
       done
     done
   done
