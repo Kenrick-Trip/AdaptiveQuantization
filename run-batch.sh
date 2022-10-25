2a39c3062a81fb154475eaf5e9a4300f9a19ce6c
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-26 01:01:42 +0200

    powers of 2 cpu, 3 mem sizes, incremented

--- run-batch.sh
+++ run-batch.sh
@@ -1,6 +1,6 @@
 for modelname in resnet18 resnet34; do
-  for cpusiz in 4 5 6; do
-    for memsiz in 1024MB 512MB; do
+  for cpusiz in 1 2 4; do
+    for memsiz in 256MB 512MB 1024MB; do
       for batchsiz in 1 2 4; do
         for quantlv in None symmetric affine histogram; do
           echo "Running $modelname with cpu: $cpusiz, memory: $memsiz, batch size: $batchsiz and quant: $quantlv"
