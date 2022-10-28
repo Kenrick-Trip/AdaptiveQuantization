f0b306531ca597a00673344612d0ea6268944e66
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-28 21:19:42 +0200

    plot regression model predictions' error

--- Analysis/anova.py
+++ Analysis/anova.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
@@ -13,9 +15,10 @@ def regression_without_interaction(data):
     table = sm.stats.anova_lm(model)
     print(table)
     print(model.summary())
+    return model
 
 
-def regression_with_pair_interactions(data):
+def regression_with_pairwise_interactions(data):
     model = ols(
         "inference_time ~ \
         C(cpu) * C(memory) + \
@@ -33,11 +36,40 @@ def regression_with_pair_interactions(data):
     table = sm.stats.anova_lm(model)
     print(table)
     print(model.summary())
+    return model
+
+
+def predict_inference_time(regression_model, X):
+    # X: (cpu, memory, batch_size, model_name, quant_scheme)
+    y_pred = regression_model.predict(X)
+    return y_pred
+
+
+def compare_true_and_pred(dataframe, model):
+    dataframe["predicted_inf_time"] = \
+        predict_inference_time(model, dataframe[["cpu", "memory", "batch_size", "model_name", "quant_scheme"]])
+    dataframe.sort_values(axis=0, by=["inference_time"])
+    y_pred = dataframe["predicted_inf_time"]
+    y_true = dataframe["inference_time"]
+    return y_pred, y_true
+
+
+def visualize_prediction_errors(y_pred, y_true):
+    diff = y_true - y_pred
+    diff.hist(bins=40)
+    plt.title('Histogram of prediction errors')
+    plt.xlabel('Inference time (ms) prediction error')
+    plt.ylabel('Frequency')
+    plt.savefig("regression_prediction_error.png")
+    plt.show()
 
 
 if __name__ == "__main__":
     df = pd.read_csv("experiment_data.csv", header=None)
     df.columns = ["cpu", "memory", "batch_size", "model_name",
                   "quant_scheme", "accuracy", "inference_time", "model_size_mb"]
-    regression_without_interaction(df)
-    # regression_with_pair_interactions(df)
+    model = regression_without_interaction(df)
+    # model = regression_with_pairwise_interactions(df)
+    y_pred, y_true = compare_true_and_pred(df, model)
+    visualize_prediction_errors(y_pred, y_true)
+
