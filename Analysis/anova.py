4c4f428c4dcd1e5728ebb87d72bd3b666c8af311
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-28 20:31:30 +0200

    pairwise interactions regression

--- Analysis/anova.py
+++ Analysis/anova.py
@@ -5,13 +5,30 @@ from statsmodels.formula.api import ols
 pd.set_option('display.max_columns', 500)
 
 
-def regression_without_interaction():
-    df = pd.read_csv("experiment_data.csv", header=None)
-    df.columns = ["cpu", "memory", "batch_size", "model_name",
-                  "quant_scheme", "accuracy", "inference_time", "model_size_mb"]
+def regression_without_interaction(data):
     model = ols(
         "inference_time ~ C(cpu) + C(memory) + C(batch_size) + C(model_name) + C(quant_scheme)",
-        data=df).fit()
+        data=data).fit()
+
+    table = sm.stats.anova_lm(model)
+    print(table)
+    print(model.summary())
+
+
+def regression_with_pair_interactions(data):
+    model = ols(
+        "inference_time ~ \
+        C(cpu) * C(memory) + \
+        C(cpu) * C(batch_size) + \
+        C(cpu) *  C(model_name) + \
+        C(cpu) *  C(quant_scheme) + \
+        C(memory) * C(batch_size) + \
+        C(memory) * C(model_name) + \
+        C(memory) * C(quant_scheme) + \
+        C(batch_size) * C(model_name) + \
+        C(batch_size) * C(quant_scheme) + \
+        C(model_name) * C(quant_scheme)",
+        data=data).fit()
 
     table = sm.stats.anova_lm(model)
     print(table)
@@ -19,4 +36,8 @@ def regression_without_interaction():
 
 
 if __name__ == "__main__":
-    regression_without_interaction()
+    df = pd.read_csv("experiment_data.csv", header=None)
+    df.columns = ["cpu", "memory", "batch_size", "model_name",
+                  "quant_scheme", "accuracy", "inference_time", "model_size_mb"]
+    regression_without_interaction(df)
+    # regression_with_pair_interactions(df)
