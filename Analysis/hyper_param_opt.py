9df4d0f197c8cdb68a525e7042ada268156f33eb
Csanád Bakos <bakoscsanad@gmail.com>
2022-10-30 20:03:21 +0100

    add 3 models, with hyper param tuning (when possible)

--- Analysis/hyper_param_opt.py
+++ Analysis/hyper_param_opt.py
@@ -1,77 +1,24 @@
-import random
-
 import numpy as np
-from sklearn import linear_model, svm, ensemble
-from sklearn.experimental import enable_halving_search_cv
-from sklearn.model_selection import HalvingRandomSearchCV
+from sklearn import linear_model
+from sklearn.model_selection import RandomizedSearchCV
 import scipy
 from scipy.stats import randint
 from sklearn.neural_network import MLPRegressor
 
 
-def tune_lr(X_train, y_train, seed=42, verbose=1, n_jobs=8):
-    lr = linear_model.LogisticRegression()
-    np.random.seed(seed)
-    compatible = {
-        "newton-cg": ["l2", "none"],
-        "lbfgs": ["l2", "none"],
-        "liblinear": ["l1", "l2"],
-        "sag": ["l2", "none"],
-        "saga": ["elasticnet", "l1", "l2", "none"]
-    }
-    best_params = None
-    best_score = 0
-    for k, v in compatible.items():
-        param_distributions = {
-            "solver": [k],
-            "penalty": v,
-            "C": scipy.stats.uniform(loc=0.0001, scale=10),  # exclude 0
-            "l1_ratio": scipy.stats.uniform()  # must be optimized otherwise we can't finetune l1 penalty properly
-        }
-        search = HalvingRandomSearchCV(lr,
-                                       param_distributions,
-                                       random_state=seed,
-                                       verbose=verbose,
-                                       n_jobs=n_jobs).fit(X_train, y_train)
-        if search.best_score_ > best_score:
-            best_score = search.best_score_
-            best_params = search.best_params_
-    return best_params
-
-
-def tune_svm(X_train, y_train, seed=42, verbose=1, n_jobs=8):
-    sv = svm.SVC(random_state=seed)
+def tune_elastic_net(X_train, y_train, seed=42, verbose=1, n_jobs=8):
+    ridge = linear_model.ElasticNet(random_state=seed)
     np.random.seed(seed)
     param_distributions = {
-        "C": scipy.stats.uniform(loc=0.0001, scale=10),  # exclude 0
-        "kernel": ["linear", "poly", "rbf", "sigmoid"],  # removed 'precomputed' option because:
-        # https://stackoverflow.com/questions/36306555/scikit-learn-grid-search-with-svm-regression
-        "gamma": scipy.stats.uniform()
+        "alpha": scipy.stats.uniform(0.0001, 1000.0),
+        "l1_ratio": scipy.stats.uniform(),
+        "selection": ["random", "cyclic"],
+        "max_iter": randint(100, 100000)
     }
-    search = HalvingRandomSearchCV(sv,
-                                   param_distributions,
-                                   random_state=seed,
-                                   verbose=verbose,
-                                   n_jobs=n_jobs).fit(X_train, y_train)
-    return search.best_params_
-
-
-def tune_rf(X_train, y_train, seed=42, verbose=1, n_jobs=8):
-    rf = ensemble.RandomForestClassifier(random_state=seed)
-    np.random.seed(seed)
-    param_distributions = {
-                          "bootstrap": [True, False],
-                          "max_depth": randint(1, len(y_train)),
-                          "max_features": scipy.stats.uniform(),
-                          "min_samples_leaf": scipy.stats.uniform(0, 0.5),
-                          "min_samples_split": scipy.stats.uniform(),
-                          "n_estimators": randint(10, 1000)
-    }
-    search = HalvingRandomSearchCV(rf,
-                                   param_distributions,
-                                   random_state=seed,
-                                   n_jobs=n_jobs,
-                                   verbose=verbose)
+    search = RandomizedSearchCV(ridge,
+                                param_distributions,
+                                n_jobs=n_jobs,
+                                verbose=verbose)
     search.fit(X_train, y_train)
     return search.best_params_
 
@@ -80,17 +27,17 @@ def tune_mlp(X_train, y_train, seed=42, verbose=1, n_jobs=8):
     mlp = MLPRegressor(random_state=seed)
     np.random.seed(seed)
     param_distributions = {
-                          "hidden_layer_sizes": randint(10, 500),
-                          "max_iter": randint(200, 500),
-                          "activation": ["identity", "logistic", "tanh", "relu"],
-                          "solver": ["lbfgs", "sgd", "adam"],
-                          "learning_rate": ["constant", "invscaling", "adaptive"],
-                          "learning_rate_init": scipy.stats.uniform(0.0001, 0.01)
+        "hidden_layer_sizes": randint(10, 500),
+        "max_iter": randint(200, 500),
+        "activation": ["identity", "logistic", "tanh", "relu"],
+        "solver": ["lbfgs", "sgd", "adam"],
+        "learning_rate": ["constant", "invscaling", "adaptive"],
+        "learning_rate_init": scipy.stats.uniform(0.0001, 0.01)
     }
-    search = HalvingRandomSearchCV(mlp,
-                                   param_distributions,
-                                   random_state=seed,
-                                   verbose=verbose,
-                                   n_jobs=n_jobs)
+    search = RandomizedSearchCV(mlp,
+                                param_distributions,
+                                random_state=seed,
+                                verbose=verbose,
+                                n_jobs=n_jobs)
     search.fit(X_train, y_train)
     return search.best_params_
--- Analysis/ml_models.py
+++ Analysis/ml_models.py
@@ -6,30 +6,23 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import r2_score
 
 from Analysis.anova import load_experiment_data, visualize_prediction_errors
-from Analysis.hyper_param_opt import tune_mlp
+from Analysis.hyper_param_opt import tune_mlp, tune_elastic_net
 
 
-def mlp_regression(X_train,
-                   y_train,
-                   hidden_layer_sizes=(100,),
-                   max_iter=200,
-                   activation="relu",
-                   solver="adam",
-                   learning_rate="constant",
-                   learning_rate_init=0.001):
-    regr = MLPRegressor(random_state=1,
-                        hidden_layer_sizes=hidden_layer_sizes,
-                        max_iter=max_iter,
-                        activation=activation,
-                        solver=solver,
-                        learning_rate=learning_rate,
-                        learning_rate_init=learning_rate_init)
+def mlp_regression(X_train, y_train, **kwargs):
+    regr = MLPRegressor(random_state=1, **kwargs)
     regr.fit(X_train, y_train)
     return regr
 
 
 def linear_regression(X_train, y_train):
     regr = linear_model.LinearRegression()
+    regr.fit(X=X_train, y=y_train)
+    return regr
+
+
+def elastic_net(X_train, y_train, **kwargs):
+    regr = linear_model.ElasticNet(**kwargs, random_state=1)
     regr.fit(X=X_train, y=y_train)
     return regr
 
@@ -45,26 +38,34 @@ def get_predictions(regr, X_test):
     return y_pred
 
 
+def run_with_and_without_tuning(regr_train_func, tune_func, data):
+    X_train, X_test, y_train, y_test = data
+
+    # get r2_score for defualt hyper parameters
+    regr = regr_train_func(X_train=X_train, y_train=y_train)
+    y_pred = get_predictions(regr, X_test)
+    default_score = r2_score(y_true=y_test, y_pred=y_pred)
+    tuned_score = None
+
+    # find better hyper parameters, get r2_score for regressor trained with these params
+    if tune_func is not None:
+        best = tune_func(X_train, y_train, seed=42, verbose=1, n_jobs=8)
+        regr = regr_train_func(X_train=X_train, y_train=y_train, **best)
+        y_pred = get_predictions(regr, X_test)
+        tuned_score = r2_score(y_true=y_test, y_pred=y_pred)
+    return default_score, tuned_score
+
+
 if __name__ == "__main__":
     df = load_experiment_data()
     X, y = split_data(df)
     ohe = OneHotEncoder()
     ohe_X = ohe.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(ohe_X, y, random_state=1)
-    # best = tune_mlp(X_train, y_train, seed=42, verbose=1, n_jobs=8)
-    # print(best)
-    # regr = mlp_regression(X_train=X_train,
-    #                       y_train=y_train,
-    #                       # hidden_layer_sizes=best["hidden_layer_sizes"],
-    #                       # max_iter=best["max_iter"],
-    #                       # activation=best["activation"],
-    #                       # solver=best["solver"],
-    #                       # learning_rate=best["learning_rate"],
-    #                       # learning_rate_init=best["learning_rate_init"]
-    #                       )
+    data = train_test_split(ohe_X, y, random_state=1)
+    regr_train_funcs = [linear_regression, elastic_net, mlp_regression]
+    tune_funcs = [None, tune_elastic_net, tune_mlp]
+    for regr_train_func, tune_func in zip(regr_train_funcs, tune_funcs):
+        default_score, tuned_score = run_with_and_without_tuning(regr_train_func, tune_func, data)
+        print("{} default and tuned r_scores: {}, {}".format(regr_train_func.__name__, default_score, tuned_score))
 
-    regr = linear_regression(X_train, y_train)
-    y_pred = get_predictions(regr, X_test)
-    print(r2_score(y_true=y_test, y_pred=y_pred))
-
-    visualize_prediction_errors(y_pred, y_test, file_name_to_save="plots/mlp_prediction_error.png")
+    # visualize_prediction_errors(y_pred, y_test, file_name_to_save="plots/mlp_prediction_error.png")
